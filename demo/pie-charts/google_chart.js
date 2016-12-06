6a325e26125dea25824f6615740fd95476d3ec56
k-kuwahara <kuwahara@lepra.jp>
2016-12-06 14:21:12 +0900

    add google charts

---
+++ demo/pie-charts/google_chart.js
@@ -0,0 +1,27 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable([
+   ["language",  "users"],
+   ["JavaScript",  1770],
+   ["Java",        570],
+   ["Objective-C", 511],
+   ["Python",      483],
+   ["Ruby",        469],
+   ["C",           217],
+   ["C++",         201],
+   ["PHP",         191],
+   ["Swift",       142],
+   ["C#",          79 ]
+  ]);
+
+  var options = {
+    title: 'hogehoge'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
+}
+
