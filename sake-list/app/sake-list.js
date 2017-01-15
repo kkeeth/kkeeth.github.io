c3f07f2043f1d01747d561788c054de13b1ee851
k-kuwahara <kuwahara@lepra.jp>
2017-01-15 20:55:03 +0900

    upgrade riot version

--- sake-list/app/sake-list.js
+++ sake-list/app/sake-list.js
@@ -11,12 +11,30 @@ exports.SAKES = [
     },
     {
         id: 2,
-        name: '獺祭（だっさい）純米大吟醸　磨き二割三分',
+        name: '獺祭　純米大吟醸　磨き二割三分',
         sp_name: '純米大吟醸',
         rice_per: 23,
         salty_sweet: '+4',
         assessment: 5,
         detail: '今まで飲んできた中ではダントツの1位'
+    },
+    {
+        id: 3,
+        name: '純米生酒 浦霞（うらかすみ）',
+        sp_name: '純米酒 生酒',
+        rice_per: 65,
+        salty_sweet: '+1.5',
+        assessment: 4,
+        detail: '飲みやすくかなり清涼感がある。でもしっかり味もする。'
+    },
+    {
+        id: 4,
+        name: '特別純米酒　田酒（でんしゅ）',
+        sp_name: '特別純米酒',
+        rice_per: 55,
+        salty_sweet: '±0',
+        assessment: 4,
+        detail: 'すっきりした味わいで料理にも合う。'
     }
 ];
 //# sourceMappingURL=sake-list.js.map
