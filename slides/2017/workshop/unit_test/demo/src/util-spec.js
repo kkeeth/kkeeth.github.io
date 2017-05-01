62645c09da5182a620f9e9ea7804da9bd94502da
k-kuwahara <kuwahara@lepra.jp>
2017-05-02 00:55:38 +0900

    minor modify

--- slides/2017/workshop/unit_test/demo/src/util-spec.js
+++ slides/2017/workshop/unit_test/demo/src/util-spec.js
@@ -1,17 +1,20 @@
 console.info('valic_check関数のテスト')
 console.info('IDの文字数が0以上かつ、Passwordがtest1234だったら正解')
 
+
 console.info('どちらも空のケース')
 if (valid_check() === 'ng1')
    console.log('OK')
 else
    console.log('NG')
 
+
 console.info('どちらも空文字列のケース')
 if (valid_check('', '') === 'ng1')
    console.log('OK')
 else
    console.error('NG')
+
 
 console.info('どちらか一方が空のケース')
 if (valid_check('hoge') === 'ng1')
@@ -22,6 +25,7 @@ if (valid_check('', 'test') === 'ng1')
    console.log('OK')
 else
    console.error('NG')
+
 
 console.info('パスワードの正解/不正解のケース')
 if (valid_check('hoge', 'test') === 'ng2')
