2e660888fbe67662a862a503f83fc9d4363ecc5f
k-kuwahara <kuwahara@lepra.jp>
2017-06-28 22:06:27 +0900

    Update app.js

--- slides/2017/workshop/unit_test/demo/src/app.js
+++ slides/2017/workshop/unit_test/demo/src/app.js
@@ -5,6 +5,11 @@ $(function() {
 			key  = auth_check(user, pass)
 
 		$('#result').text(get_message(key))
+
+		if (key === 'login')
+			$('#result').addClass('correct')
+		else
+			$('#result').removeClass('correct')
 	})
 })
 
