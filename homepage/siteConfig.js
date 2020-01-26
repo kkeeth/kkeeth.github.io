07db18f16c9ed5f26808efc026f93efe779511d9
kkeeth <zensin0082@gmail.com>
2020-01-26 13:27:21 +0900

    feat: don't manage for build dir

--- homepage/siteConfig.js
+++ homepage/siteConfig.js
@@ -21,25 +21,19 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  title: 'My HomePage', // Title for your website.
+  tagline: 'A website for my details',
+  url: 'https://keeth.me',
+  baseUrl: '/',
 
-  // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+  projectName: '',
+  organizationName: 'kkeeth',
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'doc1', label: 'Details'},
+    {doc: 'doc2', label: 'Links'},
+    {doc: 'doc4', label: 'Skills'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
@@ -54,7 +48,7 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#747179',
+    primaryColor: '#008080',
     secondaryColor: '#514f54',
   },
 
@@ -62,6 +56,7 @@ const siteConfig = {
   /*
   fonts: {
     myFont: [
+      "Josefin Sans",
       "Times New Roman",
       "Serif"
     ],
@@ -73,10 +68,9 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} kkeeth`,
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'default',
   },
 
